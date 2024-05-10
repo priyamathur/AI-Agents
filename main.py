11cd28818e88483d76826baea8d6a58d960752f5
mathurpriya19 <mathurpriya19@gmail.com>
2024-05-10 16:53:43 +0530

    SQLAgent

---
+++ main.py
@@ -0,0 +1,50 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from langchain.utilities import SQLDatabase
+from langchain.llms import OpenAI
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.agents.agent_types import AgentType
+import dotenv
+dotenv.load_dotenv()
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    db = SQLDatabase.from_uri("sqlite:///Chinook.db")
+    # check that the database has been instantiated correctly
+    db.get_usable_table_names()
+    llm = OpenAI(temperature=0, verbose=True)
+
+    agent_executor = create_sql_agent(
+        llm=llm,
+        toolkit=SQLDatabaseToolkit(db=db, llm=llm),
+        verbose=True,
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+    )
+
+    FORMAT_INSTRUCTIONS = """
+    Use the following format:
+    Question: the input question you must answer
+    Thought: you should always think about what to do
+    Action: the action to take, should be one of [{tool_names}]
+    Action Input: the input to the action
+    Observation: the result of the action
+    ... (this Thought/Action/Action Input/Observation can repeat N times)
+    Thought: I now know the final answer
+    Final Answer: the final answer to the original input question"""
+
+    # agent_executor.run(
+    #     "How many employees are there?"
+    # )
+
+    agent_executor.run("Who are the top 3 best selling artists?")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
